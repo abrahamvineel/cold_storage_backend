e526435771fb07b98d64d38692d7909583282687
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2025-01-25 11:54:09 -0500

    added spring security bcrypt

--- UserService/src/main/java/org/userservice/config/SecurityConfig.java
+++ UserService/src/main/java/org/userservice/config/SecurityConfig.java
@@ -1,2 +1,14 @@
-package org.userservice.config;public class SecurityConfig {
+package org.userservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- UserService/src/main/java/org/userservice/service/UserService.java
+++ UserService/src/main/java/org/userservice/service/UserService.java
@@ -1,6 +1,7 @@
 package org.userservice.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.userservice.dto.UserCreationRequest;
 import org.userservice.model.User;
@@ -11,11 +12,13 @@ import org.userservice.repository.UserRepository;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public void createUser(UserCreationRequest userCreationRequest) {
         User user = new User();
         user.setEmail(userCreationRequest.getEmail());
-        user.setPassword(userCreationRequest.getPassword());
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
         userRepository.save(user);
     }
 }
