914f5418d5d957c509ff743e2f173eec989de49b
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2025-01-30 20:39:28 -0500

    hash password

--- UserService/src/main/java/org/userservice/service/UserService.java
+++ UserService/src/main/java/org/userservice/service/UserService.java
@@ -1,12 +1,15 @@
 package org.userservice.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.userservice.dto.UserCreationRequest;
 import org.userservice.dto.UserLoginDetailsRequest;
 import org.userservice.model.User;
 import org.userservice.repository.UserRepository;
+
+import java.security.SecureRandom;
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +29,11 @@ public class UserService {
         User user = new User();
         user.setEmail(loginDetailsRequest.getEmail());
         String encodedPassword = passwordEncoder.encode(loginDetailsRequest.getPassword());
+
+        SecureRandom random = new SecureRandom();
+        random.generateSeed(12345);
+        String salt = BCrypt.gensalt(10, random); // Fixed salt
+
         user.setPassword(encodedPassword);
         return userRepository.doesUserExist(user.getEmail(), user.getPassword()).isPresent();
     }
