a4b253c105c711f014df21cb25f6e1a21ca6099f
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2025-02-05 20:43:24 -0500

    added file save to repository

--- FileUpload/src/main/java/org/fileupload/service/FileUploadService.java
+++ FileUpload/src/main/java/org/fileupload/service/FileUploadService.java
@@ -1,19 +1,31 @@
 package org.fileupload.service;
 
+import lombok.AllArgsConstructor;
 import org.fileupload.dto.FileUploadRequest;
+import org.fileupload.model.File;
+import org.fileupload.model.FileType;
 import org.fileupload.repository.FileUploadRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
 
 @Service
+@AllArgsConstructor
 public class FileUploadService {
-    private List<String> blackList = new ArrayList<>();
+
+    private Set<FileType> blackList;
 
     private FileUploadRepository repository;
 
     public void uploadFile(FileUploadRequest fileUploadRequest) {
-
+        File file = new File();
+        if (!blackList.contains(fileUploadRequest.getFileType())) {
+            file.setUserEmail(fileUploadRequest.getUserEmail());
+            file.setFileSizeInBytes(fileUploadRequest.getFileSizeInBytes());
+            file.setFileType(fileUploadRequest.getFileType());
+            file.setUrl(fileUploadRequest.getUrl());
+            file.setUploadDate(fileUploadRequest.getUploadDate());
+            repository.save(file);
+        }
     }
 }
