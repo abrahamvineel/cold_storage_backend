42743f400b4b7c9d1131c2568157d78903f75fb7
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2025-02-15 17:55:15 -0500

    added webconfig and securtiy config to avoid cors error

---
+++ FileUpload/src/main/java/org/fileupload/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package org.fileupload.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/**").permitAll();
+        return http.build();
+    }
+}
--- FileUpload/src/main/java/org/fileupload/controller/FileUploadController.java
+++ FileUpload/src/main/java/org/fileupload/controller/FileUploadController.java
@@ -18,7 +18,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/files")
 @AllArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000")
 public class FileUploadController {
 
     private FileUploadService fileUploadService;
