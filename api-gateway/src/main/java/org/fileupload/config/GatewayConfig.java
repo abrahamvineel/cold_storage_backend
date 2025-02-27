2c4f23e63a180e920f026f8bd8ad6749c035627b
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2025-02-26 20:30:00 -0500

    reverted oauth session

--- api-gateway/src/main/java/org/fileupload/config/GatewayConfig.java
+++ api-gateway/src/main/java/org/fileupload/config/GatewayConfig.java
@@ -12,18 +12,6 @@ public class GatewayConfig {
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("file-upload-service", r -> r.path("/files/**")
-                        .filters(f -> f
-                                .rewritePath("/files/(?<segment>.*)", "/${segment}") // Rewrite path if needed
-                                .filter((exchange, chain) -> {
-                                    String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
-                                    System.out.println("Forwarding Authorization Header: " + authHeader);
-
-                                    if (authHeader != null) {
-                                        exchange.getRequest().mutate().header("Authorization", authHeader); // Forward token
-                                    }
-                                    return chain.filter(exchange);
-                                })
-                        )
                         .uri("http://localhost:9194"))
                 .route("user-service", r -> r.path("/user/**")
                         .uri("http://localhost:9193"))
